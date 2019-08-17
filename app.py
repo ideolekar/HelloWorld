59895e43fc8b8c6fbc172624c6f4b2304a3839f8
ideolekar <ishant.deolekar.sap@gmail.com>
2019-08-17 16:38:24 +0530

    Updated.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+port = int(os.environ["PATH"])
 
 @app.route('/')
 def index():
    return '<html><body><h1>Hello World</h1></body></html>'
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(port=port, host="0.0.0.0")
